06f42d4b8261a5714db637de3d9f1a8928ed4f72
Harshad Marathe <harshadmarathe8@gmail.com>
2025-10-02 15:43:02 +0530

    initial commit

---
+++ src/main/java/com/app/onlinequizapi/OnlineQuizApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.onlinequizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineQuizApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/onlinequizapi/OnlineQuizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.onlinequizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
